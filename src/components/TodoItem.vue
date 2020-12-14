a396c954eb60a5e79e56c93c336284619ab8c8e0
Akalin <a.f.akalin@gmail.com>
2020-12-14 10:00:50 +0100

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-bind:class="{ 'is-complete': todo.completed }">
+    <p class="todo-item">
+      <input type="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete() {
+      console.log(123)
+      this.todo.completed = !this.todo.completed
+    },
+  },
+}
+</script>
+
+<style scoped>
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  background: #f4f4f4;
+  margin: 10px 1em;
+  padding: 1em 5em;
+  border-bottom: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+  background: #affe11;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus magnam est fuga aut neque molestiae minus. Libero dolores eligendi atque vero, dicta eum voluptate distinctio adipisci dolor dignissimos facere dolorum itaque, ab eos exercitationem tenetur sint corrupti sed? Dolore earum officiis sunt harum aliquam quis repellat delectus repellendus labore minus.
+    </p>
+  </div>
+</template>
